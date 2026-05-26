57b2d7b96a9b5e206ae91e075009d139132240ee
patrick <nosrallamus@gmail.com>
2026-05-26 23:54:24 +0200

    added check_dup

--- check_errors.c
+++ check_errors.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   check_errors.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paduarte <paduarte@student.42malaga.com>   +#+  +:+       +#+        */
+/*   By: paduarte <paduarte@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 10:18:25 by paduarte          #+#    #+#             */
-/*   Updated: 2026/05/26 16:22:05 by paduarte         ###   ########.fr       */
+/*   Updated: 2026/05/26 23:52:36 by paduarte         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,17 +21,36 @@ int	check_errors(char *argv[])
 	i = 1;
 	while (argv[i])
 	{
-		if ((ft_atoll(argv[i]) > INT_MAX) || ft_atoll(argv[i]) == 0)
-			{
-                ft_printf("Error\n");
-                return(2);
-            }
-		i++;		
+		if ((ft_atoll(argv[i]) > INT_MAX) || ft_atoll(argv[i]) == 0
+			|| check_dup(argv))
+		{
+			ft_printf("Error\n");
+			return (2);
+		}
+		i++;
 	}
 	return (0);
 }
 
-//argv[i] es solo numero??: 
+int	check_dup(char *argv[])
+{
+	size_t	i;
+	size_t	j;
+
+	i = 1;
+	while (argv[i])
+	{
+		j = i + 1;
+		while (argv[j])
+		{
+			if (!ft_strcmp(argv[i], argv[j]))
+				return (2);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
 
 /*static int  show_error(void)
 {
